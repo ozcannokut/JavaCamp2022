4d64cb95c39d3037df3a6518e9a83c1397d711a4
özcan okut <65574411+ozcannokut@users.noreply.github.com>
2022-10-05 21:01:56 +0300

    Delete classesWithAttributes directory

--- classesWithAttributes/src/ProductManager.java
+++
@@ -1,9 +0,0 @@
-public class ProductManager {
-    public void Add(Product product){
-        //JDBS
-        System.out.println("Urun eklendi "+ product.getName());
-    }
-    //public void Add2(int id ,String name,String description,int stockAmount,double price,)
-
-
-}
